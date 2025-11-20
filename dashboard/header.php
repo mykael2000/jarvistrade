7b876c16c106d5727ba0283121f286108dd5e01b
Michael <eramehmichael2000@gmail.com>
2025-11-20 20:04:54 +0100

    updte

--- dashboard/header.php
+++ dashboard/header.php
@@ -565,7 +565,7 @@ include("function.php");
             </div>
             <ul class="space-y-1">
                 <li>
-                    <a href="dashboard.php"
+                    <a href="index.php"
                        class="flex items-center px-3 py-2 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-blue-50 dark:hover:bg-blue-900/50 transition-colors duration-150 bg-blue-50 dark:bg-blue-900/50 text-blue-600 dark:text-blue-400 font-medium">
                         <i data-lucide="layout-dashboard" class="w-5 h-5 mr-3"></i>
                         Dashboard
