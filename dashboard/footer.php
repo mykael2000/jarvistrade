2ff4a0d5aa2973ac67d020d8cc1f1937814f709c
Michael <eramehmichael2000@gmail.com>
2025-11-21 10:47:31 +0100

    updte

--- dashboard/footer.php
+++ dashboard/footer.php
@@ -1,2 +1,15 @@
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '8376b71ab357b9a0cdb5e4d2ded7799579f3d1ef';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </body>
 </html>
